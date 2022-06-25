c275aeaaf53248a07caeafcec8a5c393d534f34c
analizajb <96861913+analizajb@users.noreply.github.com>
2022-06-25 11:01:02 -0700

    adding content to model index.js

---
+++ models/index.js
@@ -0,0 +1,24 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+module.exports = { 
+    User, 
+    Post, 
+    Comment 
+};
